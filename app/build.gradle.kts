b737decc6a68e6b87d2a6069df93d0035197e3de
David Hernández <daves98hg@hotmail.com>
2024-06-25 22:46:44 -0500

    Add project dependencies, config signing, config build and res secret values, add Firebase google-services.json and config maps key on AndroidManifest.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,13 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.room)
+    alias(libs.plugins.dagger.hilt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -12,7 +19,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -20,7 +27,34 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias      = gradleLocalProperties(rootDir, rootProject.providers)
+                                .getProperty("KEY_ALIAS")
+            keyPassword   = gradleLocalProperties(rootDir, rootProject.providers)
+                                .getProperty("KEY_PASSWORD")
+            storeFile     = file(
+                                gradleLocalProperties(rootDir, rootProject.providers)
+                                    .getProperty("KEY_STORE_FILE")
+                            )
+            storePassword = gradleLocalProperties(rootDir, rootProject.providers)
+                                .getProperty("KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
+        all {
+            val googleMapsApiKey = gradleLocalProperties(rootDir, rootProject.providers)
+                .getProperty("KEY_GOOGLE_MAPS")
+            val placesApiKey = gradleLocalProperties(rootDir, rootProject.providers)
+                .getProperty("KEY_PLACES")
+            val geminiApiKey = gradleLocalProperties(rootDir, rootProject.providers)
+                .getProperty("KEY_GEMINI")
+            resValue("string","GOOGLE_MAPS_API_KEY", "\"$googleMapsApiKey\"")
+            buildConfigField("String","PLACES_API_KEY", "\"$placesApiKey\"")
+            buildConfigField("String","GEMINI_API_KEY", "\"$geminiApiKey\"")
+            signingConfig = signingConfigs.getByName("release")
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
@@ -38,32 +72,59 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
     }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    room {
+        schemaDirectory("$projectDir/schemas")
+    }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+    // Splash Screen
+    implementation(libs.androidx.splashscreen)
+    // Compose
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.material3)
+    implementation(libs.compose.navigation)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    // Maps compose
+    implementation(libs.google.maps.compose)
+    // Icons
+    implementation(libs.androidx.material.icons.extended)
+    // Kotlin
+    implementation(libs.kotlin.serialization)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.core)
+    // Ktor
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.logging)
+    // Room
+    implementation(libs.room.ktx)
+    implementation(libs.room.runtime)
+    ksp(libs.room.compiler)
+    // Dagger Hilt
+    implementation(libs.dagger.hilt.android)
+    ksp(libs.dagger.hilt.android.compiler)
+    // Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics.ktx)
+    implementation(libs.firebase.firestore.ktx)
+    // Timber logging
+    implementation(libs.timber)
+    //
+    debugImplementation(libs.compose.ui.tooling)
 }
