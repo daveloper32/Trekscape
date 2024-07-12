7ac52936dd7c3ccc747596cd7c4150322edac7d0
Estarly Amaya <estarlyjesusamaya@gmail.com>
2024-07-11 22:11:21 -0500

    - BottomBar was created - TopBar was created

---
+++ app/src/main/java/com/spherixlabs/trekscape/home/presentation/components/BottomBarHome.kt
@@ -0,0 +1,78 @@
+package com.spherixlabs.trekscape.home.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.NotListedLocation
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.spherixlabs.trekscape.core.presentation.ui.theme.TrekScapeTheme
+import com.spherixlabs.trekscape.home.domain.enums.HomeType
+
+@Composable
+fun BottomBarHome(onItemClicked  : ( HomeType) -> Unit, ){
+    Row(
+        modifier = Modifier
+            .safeDrawingPadding()
+            .padding(20.dp)
+            .clip(CircleShape)
+            .background(Color.White)
+            .padding(5.dp)
+            .fillMaxWidth()
+
+    ) {
+        Icon(
+            Icons.Rounded.Home,
+            contentDescription = "",
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .size(35.dp)
+                .weight(1f)
+                .clickable { onItemClicked(HomeType.HISTORY) }
+        )
+        Icon(
+            Icons.AutoMirrored.Rounded.NotListedLocation,
+            contentDescription = "",
+            tint     = Color.White,
+            modifier = Modifier
+                .clip(CircleShape)
+                .background(MaterialTheme.colorScheme.secondary)
+                .padding(20.dp)
+                .size(35.dp)
+                .clickable { onItemClicked(HomeType.RECOMMENDATIONS) }
+        )
+        Icon(
+            Icons.Rounded.Person,
+            contentDescription = "",
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .size(35.dp)
+                .weight(1f)
+                .clickable { onItemClicked(HomeType.PROFILE) }
+        )
+    }
+}
+@Preview
+@Composable
+private fun BottomBarHomePreview() {
+    TrekScapeTheme {
+        BottomBarHome{}
+    }
+}
---
+++ app/src/main/java/com/spherixlabs/trekscape/home/presentation/components/TopBarHome.kt
@@ -0,0 +1,55 @@
+package com.spherixlabs.trekscape.home.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.spherixlabs.trekscape.R
+
+@Composable
+fun TopBarHome(name : String){
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier          = Modifier
+            .safeDrawingPadding()
+            .padding(10.dp)
+            .clip(CircleShape)
+            .background(Color.White)
+            .padding(horizontal = 15.dp, vertical = 5.dp)
+    ) {
+        Image(
+            painter            = painterResource(id = R.drawable.ic_logo),
+            contentDescription = "",
+            modifier           = Modifier
+                .padding(end = 10.dp)
+                .size(40.dp)
+                .clip(CircleShape))
+        Column {
+            Text(
+                text  = stringResource(id = R.string.lab_hi),
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Text(
+                text  = name,
+                style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+    }
+}
